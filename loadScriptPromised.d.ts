a0125e3c64a34c8b3467796752b0bceffa063ddf
Brian John Hanna <bjhanna728@gmail.com>
2024-07-23 09:54:34 -0400

    Fix TS function args definition for loadScriptPromised

--- loadScriptPromised.d.ts
+++ loadScriptPromised.d.ts
@@ -1,2 +1,2 @@
 // Needs a name due to a bug in the ESLint TS parser
-export default function defaultExport(url: string): Promise<void>;
+export default function defaultExport(url: string, attrs?: object): Promise<void>;
